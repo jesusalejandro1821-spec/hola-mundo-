c1a9ed7a59967d0f3db6adfff484c66f39c15e63
alejandro <jesus.alejando1821@gmail.com>
2025-10-25 15:01:32 -0400

    crear componentes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Vehiculos from './components/Vehiculos'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() 
+{
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>venta de coches</h1>
+      <Vehiculos description='coche 1' precio={500}  agencia='lagos' imagen='https://cdn.topgear.es/sites/navi.axelspringer.es/public/media/image/2024/10/prueba-aston-martin-vanquish-4256121.jpg?tf=1920x'/> 
+      <Vehiculos description='coche 2' precio={5000} agencia='perro' imagen='https://th.bing.com/th/id/R.43318d7b8db2824b52561915c0e6bd2f?rik=r3c3yzt4JG2fig&riu=http%3a%2f%2fwww.autopista.es%2fmedia%2fcache%2foriginal%2fupload%2fimages%2fimagegallery%2fimagegallery-45597-564c91d585489.jpg&ehk=%2fjWqIzFybmmAjpqAzX4cCy8r3GXziUqNIsZuKSFmhfY%3d&risl=&pid=ImgRaw&r=0'/>  
+      <Vehiculos description='coche 3' precio={200} agencia='las joyas' imagen='https://th.bing.com/th/id/R.56356441b87f55fa9e981f888b2e9b5e?rik=SPffjbS9jtrljg&riu=http%3a%2f%2fwww.autopista.es%2fmedia%2fcache%2foriginal%2fupload%2fimages%2fimagegallery%2fimagegallery-45597-564c91cee3d2a.jpg&ehk=LRSD%2f4NoJsMY2Qi7txioIBgTVtnwu6sKtTukRA%2f0V84%3d&risl=&pid=ImgRaw&r=0'/>  
+      
+
     </>
   )
 }
